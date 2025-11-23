ac44e3ba04d665254faec44c86e7b379edb46597
biplovmaharjan-oss <biplovmaharjan12@gmail.com>
2025-11-23 21:24:47 +0545

    added portfolio

---
+++ week1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Task1{
+    public static void main(string[]args)
+        Scanner sc = new Scanner(system.in)
+        system.out.println("enter a number");
+        int num = sc.nextInt();
+}
